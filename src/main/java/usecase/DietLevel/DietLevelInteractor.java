db7f119dca44dfb310435be60fbe3f47304fb3b0
RuohanW1 <rh.wang@mail.utoronto.ca>
2024-11-18 17:27:02 -0500

    adding interfaces-adapter

--- src/main/java/usecase/DietLevel/DietLevelInteractor.java
+++ src/main/java/usecase/DietLevel/DietLevelInteractor.java
@@ -1,7 +1,10 @@
 package usecase.DietLevel;
 
-public class DietLevelInteractor implements DietLevelInputBoundary{
+public class DietLevelInteractor{
+
+    /*
     private final DietLevelUserDataAccessInterface dietLevelUserDataAccessInterface;
     private final DietLevelOutputBoundary inputBoundary;
     private final
+     */
 }
