3f57421d0084f5ea1c707c677ec382d8a73fbd6a
xiran <xiran.yin@mail.utoronto.ca>
2024-11-18 16:59:02 -0500

    random usecase

--- src/main/java/usecase/Random/RandomInteractor.java
+++ src/main/java/usecase/Random/RandomInteractor.java
@@ -1,4 +1,32 @@
 package usecase.Random;
 
-public class RandomInteractor {
+import api.RecipeDataBase;
+import entity.Recipe;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RandomInteractor implements RandomInputBoundary {
+    private final RecipeDataBase recipeDataBase;
+    private final RandomOutputBoundary randomOutputBoundary;
+
+    public RandomInteractor(RecipeDataBase recipeDataBase, RandomOutputBoundary randomOutputBoundary) {
+        this.recipeDataBase = recipeDataBase;
+        this.randomOutputBoundary = randomOutputBoundary;
+    }
+
+    @Override
+    public void searchKeywordRecipe(RandomInputData randomInputData) {
+        try {
+            List<Recipe> recipes = recipeDataBase.getAllRecipes(
+                    randomInputData.getKeyword(), null, null, 0, 0
+            );
+            RandomOutputData randomOutputData = new RandomOutputData(recipes);
+            randomOutputBoundary.presentRecipes(randomOutputData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            randomOutputBoundary.presentRecipes(new RandomOutputData(Collections.emptyList()));
+        }
+
+    }
 }
--- src/main/java/usecase/Random/RandomOutputData.java
+++ src/main/java/usecase/Random/RandomOutputData.java
@@ -1,4 +1,15 @@
 package usecase.Random;
 
+import entity.Recipe;
+
+import java.util.List;
+
 public class RandomOutputData {
+    private final List<Recipe> recipes;
+    public RandomOutputData(List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
 }
