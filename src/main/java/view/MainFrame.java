4826c8a5812673b941f6d8cb19e616dccde8d721
EmmaYimingChen <emmaymchen@outlook.com>
2024-11-24 15:21:48 -0500

    home view & main Frame -- prettier button

--- src/main/java/view/MainFrame.java
+++ src/main/java/view/MainFrame.java
@@ -23,7 +23,7 @@ public class MainFrame extends JFrame {
                      KeywordOutputBoundary keywordOutputBoundary,
                      CaloriesInputBoundary caloriesInputBoundary) {
         setTitle("Recipe Finder");
-        setSize(800, 600);
+        setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
