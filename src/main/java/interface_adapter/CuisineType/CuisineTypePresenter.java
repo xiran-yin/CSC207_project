f790fb7157c55c9e44c4ffeb39737d6e079e79fc
Beryl Guo <berylguo@beryls-mbp.home>
2024-11-24 23:18:32 -0500

    refectored for controller and presenter

--- src/main/java/interface_adapter/CuisineType/CuisineTypePresenter.java
+++ src/main/java/interface_adapter/CuisineType/CuisineTypePresenter.java
@@ -1,4 +1,23 @@
 package interface_adapter.CuisineType;
 
-public class CuisineTypePresenter {
+import entity.Recipe;
+import usecase.CuisineType.CuisineTypeOutputBoundary;
+import usecase.CuisineType.CuisineTypeOutputData;
+import view.RecipeChoiceView;
+
+import java.util.List;
+
+public class CuisineTypePresenter implements CuisineTypeOutputBoundary {
+    private final RecipeChoiceView recipeChoiceView;
+
+    public CuisineTypePresenter(RecipeChoiceView recipeChoiceView) {
+        this.recipeChoiceView = recipeChoiceView;
+    }
+
+    @Override
+    public void presentRecipesCuisine(CuisineTypeOutputData cuisineTypeOutputData) {
+        List<Recipe> recipes = cuisineTypeOutputData.getRecipes();
+        recipeChoiceView.setCuisineRecipes(recipes);
+        recipeChoiceView.displayRecipes();
+    }
 }
